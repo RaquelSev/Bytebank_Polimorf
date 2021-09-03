aed307e8dfff58172be52545fa7e607eba42e0cd
Raquel Severo <raquel.sev.celia@gmail.com>
2021-09-03 00:41:16 -0300

    Adição de comentários para fins didáticos

--- src/ControleBonificacao.java
+++ src/ControleBonificacao.java
@@ -4,10 +4,22 @@ public class ControleBonificacao {
 
     //Como gerente e editor de video sao funcionarios, posso usar a referencia mais generica para usar o método - eles herdam
     //O metodo que será chamado sera o da classe que está chamando
-    public void registra(Funcionario g){
-        double bonif = g.getBonificacao();
+    
+    public void registra(Funcionario f){
+        double bonif = f.getBonificacao();
         this.soma += bonif;
      }
+
+    //Para fins didáticos - não precisaria de um método de bonificacao para cada Classe
+    //  public void registra(Gerente g){
+    //     double bonif = g.getBonificacao();
+    //     this.soma += bonif;
+    //  }
+
+    //  public void registra(EditorVideo ev){
+    //     double bonif = ev.getBonificacao();
+    //     this.soma += bonif;
+    //  }
 
     public double getSoma(){
         return soma;
